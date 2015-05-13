ac47abc795fb7eed5f16ebeacc350bcfc40e5e9f
Zach Chaney <zachary.chaney@gmail.com>
2015-05-13 16:53:23 -0500

    submitting updates

--- dist/main.js
+++ dist/main.js
@@ -1,34 +1,35 @@
 function* guessingGame() {
 
-var complete = false;
-var answer = null;
-var numRandom = Math.floor(Math.random() * 100);
+	var complete = false;
+	var answer = null;
+	var numRandom = Math.floor(Math.random() * 100);
 
-yield game.say('Welcome to the number guessing game!');
+	yield game.say('Welcome to the number guessing game!');
 
-var myName = yield game.ask('What\'s your name?');
+	var myName = yield game.ask('What\'s your name?');
 
-yield game.say("Let's begin, "+myName)+"!";
+	yield game.say("Let's begin, "+myName)+"!";
 
-console.log(numRandom);
+	console.log(numRandom);
 
-do {
-	answer = yield game.ask('Choose your answer!');
-	var numAnswer = parseInt(answer, 10);
-	console.log(answer);
+	do {
+		answer = yield game.ask('Choose your answer!');
+		var numAnswer = parseInt(answer, 10);
+		console.log(answer);
+		if (numAnswer === numRandom) {
+			complete = true;
+		}
+		else if (numAnswer > numRandom) {
+		yield game.say('Looks like you\'re a tad high! Let\'s play again!');
+		}
+		else {
+		yield game.say('Looks like you\'re a tad low! Let\'s play again!');
+		}
+	} while(!complete)
+	
 	if (numAnswer === numRandom) {
-		complete = true;
-}
-	else {
-	yield game.say('Sorry, incorrect. Let\'s play again!');
-}
-
-} while(!complete)
-
-if (numAnswer === numRandom) {
-	yield game.say('Congratulations! You guessed correctly!');
-}
-
+		yield game.say('Congratulations! You guessed correctly!');
+	}
 }
 
 
