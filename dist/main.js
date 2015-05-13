d1322392e7457a020008035faf76b963cc6dc37d
Zach Chaney <zachary.chaney@gmail.com>
2015-05-13 14:52:32 -0500

    first final version

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,35 @@
 function* guessingGame() {
-	console.log(yield game.say('Hey there!'));
-	console.log(yield game.say('Woah', 'This is cool'));
-	console.log(yield game.ask('Woah', 'This is cool'));
-	console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
-	console.log(yield game.say('That\'s all folks!', 'THE END'));
+
+var complete = false;
+var answer = parseInt(null);
+var numRandom = Math.floor(Math.random() * 100);
+
+yield game.say('Welcome to the number guessing game!');
+
+var myName = yield game.ask('What\'s your name?');
+
+yield game.say("Let's begin, "+myName)+"!";
+
+console.log(numRandom);
+
+do {
+	answer = yield game.ask('Choose your answer!');
+	console.log(answer);
+	if(answer === numRandom) {
+		complete = true;
+	}
+} 
+
+while(!complete)
+//while(complete === false)
+
+if(answer === numRandom) {
+	yield game.say('Sorry, incorrect! Let\'s play again!');
 }
+else {
+	yield game.say('Congratulations! You guessed correctly!');
+}
+
+}
+
+
